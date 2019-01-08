e6f06f9db351ba739d27e3828741819b1c880516
GermainDuran <arturodurantorres@gmail.com>
2019-01-08 17:39:24 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      //Its initial state should have a property called secondsLeft.
+      //The initial value of secondsLeft is set by passing in an initialCount prop to
+      //the Bomb component. Don't forget to pass the argument props into the constructor
+      //(i.e., constructor(props)).
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    //It should render the text '<SECONDS_LEFT> seconds left before I go boom!', where
+    // <SECONDS_LEFT> is the value of secondsLeft. If secondsLeft equals 0, it should render 'Boom!' instead.
+    if (this.state.secondsLeft === 0) {
+      return (<p>Boom!</p>)
+    } else {
+      return (<p>{this.state.secondsLeft} seconds left before I go boom!</p>)
+    }
+  }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,31 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+//esto es un Component, y coloco export para poder importarlo en otro documento
+export default class ImageSlider extends React.Component {
+  constructor() {
+    //debo trabajar con super siempre en este paso
+    // Its initial state should have a property called currentSlideIndex that starts at 0
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+
+
+  render() {
+   return (
+     //It should only render out the text 'I am on slide <CURRENT_SLIDE>', where <CURRENT_SLIDE>
+     //is the value of this.state.currentSlideIndex.
+     <p>I am on slide {this.state.currentSlideIndex}</p>
+    )
+  }
+}
+
+// nota acerca de this 
+// 1. this within a function called with the keyword new in front will point to the newly created object
+// 2. this within a function called with apply/call/bind will be the object passed as the first parameter
+// 3. this within a function called with a context (i.e. Object.method()) will be the object.
+// 4. this for a simple function call (fn()) will be the window (browser) or the global object (Node).
+// If we are in strict mode this will be undefined for simple function calls
